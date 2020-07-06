63aabbe4f0804c1233787742fabaa6046d1fb23b
mbrad26 <mbrad26@gmail.com>
2020-07-06 09:03:45 +0100

    Airport class instructs a plane to land

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,12 @@
 class Airport
+  attr_reader :planes
+
+  def initialize
+    @planes = []
+  end
+
   def land(plane)
+    planes << plane
   end
 
   def take_off(plane)
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,15 @@ describe Airport do
   it { is_expected.to respond_to(:land).with(1).argument }
 
   it { is_expected.to respond_to(:take_off).with(1).argument }
+
+  describe '#land' do
+    it 'instructs a plane to land' do
+      airport = described_class.new
+      plane = instance_double('Plane')
+
+      airport.land(plane)
+
+      expect(airport.planes).to include plane
+    end
+  end
 end
