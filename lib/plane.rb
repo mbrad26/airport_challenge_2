7c475d45bbcff5acde7c9dc92e0460b6b800d397
mbrad26 <mbrad26@gmail.com>
2020-07-06 16:43:29 +0100

    Refactored code in Plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -5,18 +5,22 @@ class Plane
   end
 
   def take_off
-    raise 'Plane already flying!' if @flying
+    raise 'Plane already flying!' if flying
   end
 
   def land(airport)
-    raise 'Plane already landed!' unless @flying
+    raise 'Plane already landed!' unless flying
 
     @flying = false
     @airport = airport
   end
 
   def airport
-    raise 'Plane is flying!' if @flying
+    raise 'Plane is flying!' if flying
     @airport
   end
+
+  private
+
+  attr_reader :flying
 end
