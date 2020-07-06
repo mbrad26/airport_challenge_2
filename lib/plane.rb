d17c62f08cf535347494651427a89fe30ace67ef
mbrad26 <mbrad26@gmail.com>
2020-07-06 15:14:43 +0100

    #airport raises an error when plane is flying

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,4 +2,8 @@ class Plane
   def take_off
     raise 'Plane already flying!'
   end
+
+  def airport
+    raise 'Plane is flying!'
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,13 +4,24 @@ describe Plane do
   subject(:plane) { described_class.new }
 
   it { is_expected.to respond_to(:take_off) }
+  it { is_expected.to respond_to(:airport) }
 
   describe '#take_off' do
     context 'when plane is flying' do
       it 'raises an error' do
         error_message = 'Plane already flying!'
-        
+
         expect { plane.take_off }.to raise_error error_message
+      end
+    end
+  end
+
+  describe '#airport' do
+    context 'when plane is flying' do
+      it 'raises an error' do
+        error_message = 'Plane is flying!'
+
+        expect { plane.airport }.to raise_error error_message
       end
     end
   end
