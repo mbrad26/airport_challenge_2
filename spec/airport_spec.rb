4f66efa4b72c2b30948ce13148762c48348de903
mbrad26 <mbrad26@gmail.com>
2020-07-06 09:18:30 +0100

    Refactored airport_spec.rb to DRY up code

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,15 +1,14 @@
 require 'airport'
 
 describe Airport do
-  it { is_expected.to respond_to(:land).with(1).argument }
+  let(:airport) { described_class.new }
+  let(:plane) { instance_double('Plane') }
 
+  it { is_expected.to respond_to(:land).with(1).argument }
   it { is_expected.to respond_to(:take_off).with(1).argument }
 
   describe '#land' do
     it 'instructs a plane to land' do
-      airport = described_class.new
-      plane = instance_double('Plane')
-
       airport.land(plane)
 
       expect(airport.planes).to include plane
@@ -18,9 +17,6 @@ describe Airport do
 
   describe '#take_off' do
     it 'instructs a plane to take_off' do
-      airport = described_class.new
-      plane = instance_double('Plane')
-
       airport.land(plane)
       airport.take_off(plane)
 
