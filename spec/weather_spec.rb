f6d3c0b03a6275f343b3646118364d29176b0534
mbrad26 <mbrad26@gmail.com>
2020-07-06 14:57:02 +0100

    Plane class is responding to #take_off

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,7 +1,7 @@
 require 'weather'
 
 describe Weather do
-  # it { is_expected.to respond_to(:stormy?) }
+  it { is_expected.to respond_to(:stormy?) }
 
   describe '#stormy?' do
     context 'when weather is stormy' do
